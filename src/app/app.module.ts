8a5f7d4e0963e9d192fd1ae09e7604b15a3c95b6
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-10-20 19:14:26 +0530

    initial commit - configured app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './ticketing/components/login/login.component';
+import { SignupComponent } from './ticketing/components/signup/signup.component';
 
+// PrimeNG imports
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MenubarModule } from 'primeng/menubar';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    // PrimeNG modules
+    ButtonModule,
+    ReactiveFormsModule,
+    MenubarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
