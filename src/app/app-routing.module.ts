1604388bbe54a7307a7a2ca0761fd441482205eb
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-10-30 00:14:01 +0530

    routing for login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupCustomerComponent } from './ticketing/components/signup-customer/signup-customer.component';
 import { SignupVendorComponent } from './ticketing/components/signup-vendor/signup-vendor.component';
+import { LoginComponent } from './ticketing/components/login/login.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'register_vendor',
     component: SignupVendorComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: 'vendor',
