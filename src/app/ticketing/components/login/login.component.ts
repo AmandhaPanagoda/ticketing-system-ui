6c4c3f577d053c60b4ef487521ceab53a1ca73ae
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-10-30 00:13:37 +0530

    login component and routing

--- src/app/ticketing/components/login/login.component.ts
+++ src/app/ticketing/components/login/login.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  validateForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  submitForm() {
+    this.authService
+      .login(this.validateForm.get('username')!.value, this.validateForm.get('password')!.value)
+      .subscribe({
+        next: res => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Login successful',
+            life: 3000,
+          });
+          this.router.navigate(['/']);
+        },
+        error: error => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Invalid credentials. Please try again.',
+            life: 3000,
+          });
+          console.error('Login error:', error);
+        },
+      });
+  }
 }
--- src/app/ticketing/services/auth/auth.service.ts
+++ src/app/ticketing/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 const BASIC_URL = 'http://localhost:8080/api/v1/auth';
+export const AUTH_HEADER = 'Authorization';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,21 @@ export class AuthService {
 
   registerVendor(signupDTO: any): Observable<any> {
     return this.http.post(`${BASIC_URL}/vendor/signup`, signupDTO);
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post(`${BASIC_URL}/authenticate`, { username, password }, { observe: 'response' })
+      .pipe(
+        map((res: HttpResponse<any>) => {
+          localStorage.setItem('user', JSON.stringify(res.body));
+          const tokenLength = res.headers.get(AUTH_HEADER)?.length;
+          const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
+          if (bearerToken) {
+            localStorage.setItem('token', bearerToken);
+          }
+          return res.body;
+        })
+      );
   }
 }
