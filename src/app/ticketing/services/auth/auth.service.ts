2d4d2f424ce87bac58ec319ee8287c493c95cd95
AmandhaPanagoda <amandhaisiri@gmail.com>
2024-10-30 20:26:50 +0530

    continued jwt auth

--- src/app/ticketing/services/auth/auth.service.ts
+++ src/app/ticketing/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
+import { UserStorageService } from '../storage/user-storage.service';
 const BASIC_URL = 'http://localhost:8080/api/v1/auth';
 export const AUTH_HEADER = 'Authorization';
 
@@ -8,7 +9,7 @@ export const AUTH_HEADER = 'Authorization';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private userStorage: UserStorageService) {}
 
   registerCustomer(signupDTO: any): Observable<any> {
     return this.http.post(`${BASIC_URL}/signup`, signupDTO);
@@ -23,11 +24,11 @@ export class AuthService {
       .post(`${BASIC_URL}/authenticate`, { username, password }, { observe: 'response' })
       .pipe(
         map((res: HttpResponse<any>) => {
-          localStorage.setItem('user', JSON.stringify(res.body));
+          this.userStorage.saveUser(res.body);
           const tokenLength = res.headers.get(AUTH_HEADER)?.length;
           const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
           if (bearerToken) {
-            localStorage.setItem('token', bearerToken);
+            this.userStorage.saveToken(bearerToken);
           }
           return res.body;
         })
--- src/app/ticketing/services/storage/user-storage.service.ts
+++ src/app/ticketing/services/storage/user-storage.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
 
+const USER = 's_user';
+const TOKEN = 's_token';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserStorageService {
+  constructor() {}
 
-  constructor() { }
+  public saveToken(token: string): void {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.setItem(TOKEN, token);
+  }
+
+  static getToken(): string | null {
+    return localStorage.getItem(TOKEN);
+  }
+
+  public saveUser(user: any): void {
+    window.localStorage.removeItem(USER);
+    window.localStorage.setItem(USER, JSON.stringify(user));
+  }
+
+  static getUser(): any {
+    const userStr = localStorage.getItem(USER);
+    return userStr ? JSON.parse(userStr) : null;
+  }
+
+  static getUserId(): string {
+    const user = this.getUser();
+    if (user) {
+      return user.id;
+    }
+    return '';
+  }
+
+  static getUserRole(): string {
+    const user = this.getUser();
+    if (user) {
+      return user.role;
+    }
+    return '';
+  }
+
+  static isCustomerLoggedIn(): boolean {
+    if (!this.getToken()) {
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role === 'CUSTOMER';
+  }
+
+  static isVendorLoggedIn(): boolean {
+    if (!this.getToken()) {
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role === 'VENDOR';
+  }
+
+  static signOut(): void {
+    window.localStorage.removeItem(USER);
+    window.localStorage.removeItem(TOKEN);
+  }
 }
